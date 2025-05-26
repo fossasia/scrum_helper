58ba1f1b767d0b809163ca57fc52065eda8166eb
Vedansh Saini <77830698+vedansh-5@users.noreply.github.com>
2025-05-26 12:23:46 +0530

    changed var to let for better scoping
    Signed-off-by: Vedansh Saini <77830698+vedansh-5@users.noreply.github.com>

--- src/scripts/scrumHelper.js
+++ src/scripts/scrumHelper.js
@@ -1,38 +1,38 @@
-var refreshButton_Placed = false;
-var enableToggle = true;
+let refreshButton_Placed = false;
+let enableToggle = true;
 function allIncluded(outputTarget = 'email') {
 
 	/* global $*/
-	var scrumBody = null;
-	var scrumSubject = null;
-	var startingDate = '';
-	var endingDate = '';
-	var githubUsername = '';
-	var projectName = '';
-	var lastWeekArray = [];
-	var nextWeekArray = [];
-	var reviewedPrsArray = [];
-	var githubIssuesData = null;
-	var lastWeekContribution = false;
-	var yesterday = false;
-	var githubPrsReviewData = null;
-	var githubUserData = null;
-	var githubPrsReviewDataProcessed = {};
-	var showOpenLabel = true;
-	var showClosedLabel = true;
-	var userReason = '';
+	let scrumBody = null;
+	let scrumSubject = null;
+	let startingDate = '';
+	let endingDate = '';
+	let githubUsername = '';
+	let projectName = '';
+	let lastWeekArray = [];
+	let nextWeekArray = [];
+	let reviewedPrsArray = [];
+	let githubIssuesData = null;
+	let lastWeekContribution = false;
+	let yesterday = false;
+	let githubPrsReviewData = null;
+	let githubUserData = null;
+	let githubPrsReviewDataProcessed = {};
+	let showOpenLabel = true;
+	let showClosedLabel = true;
+	let userReason = '';
 
-	var pr_merged_button =
+	let pr_merged_button =
 		'<div style="vertical-align:middle;display: inline-block;padding: 0px 4px;font-size:9px;font-weight: 600;color: #fff;text-align: center;background-color: #6f42c1;border-radius: 3px;line-height: 12px;margin-bottom: 2px;" class="State State--purple">closed</div>';
-	var pr_unmerged_button =
+	let pr_unmerged_button =
 		'<div style="vertical-align:middle;display: inline-block;padding: 0px 4px;font-size:9px;font-weight: 600;color: #fff;text-align: center;background-color: #2cbe4e;border-radius: 3px;line-height: 12px;margin-bottom: 2px;"  class="State State--green">open</div>';
 
-	var issue_closed_button =
+	let issue_closed_button =
 		'<div style="vertical-align:middle;display: inline-block;padding: 0px 4px;font-size:9px;font-weight: 600;color: #fff;text-align: center;background-color: #6f42c1;border-radius: 3px;line-height: 12px;margin-bottom: 2px;" class="State State--purple">closed</div>';
-	var issue_opened_button =
+	let issue_opened_button =
 		'<div style="vertical-align:middle;display: inline-block;padding: 0px 4px;font-size:9px;font-weight: 600;color: #fff;text-align: center;background-color: #2cbe4e;border-radius: 3px;line-height: 12px;margin-bottom: 2px;"  class="State State--green">open</div>';
 
-	var linkStyle = '';
+	let linkStyle = '';
 	function getChromeData() {
 		chrome.storage.local.get(
 			[
@@ -114,12 +114,12 @@ function allIncluded(outputTarget = 'email') {
 		startingDate = getYesterday();
 	}
 	function getLastWeek() {
-		var today = new Date();
-		var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
-		var lastWeekMonth = lastWeek.getMonth() + 1;
-		var lastWeekDay = lastWeek.getDate();
-		var lastWeekYear = lastWeek.getFullYear();
-		var lastWeekDisplayPadded =
+		let today = new Date();
+		let lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+		let lastWeekMonth = lastWeek.getMonth() + 1;
+		let lastWeekDay = lastWeek.getDate();
+		let lastWeekYear = lastWeek.getFullYear();
+		let lastWeekDisplayPadded =
 			('0000' + lastWeekYear.toString()).slice(-4) +
 			'-' +
 			('00' + lastWeekMonth.toString()).slice(-2) +
@@ -128,12 +128,12 @@ function allIncluded(outputTarget = 'email') {
 		return lastWeekDisplayPadded;
 	}
 	function getToday() {
-		var today = new Date();
-		var Week = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-		var WeekMonth = Week.getMonth() + 1;
-		var WeekDay = Week.getDate();
-		var WeekYear = Week.getFullYear();
-		var WeekDisplayPadded =
+		let today = new Date();
+		let Week = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+		let WeekMonth = Week.getMonth() + 1;
+		let WeekDay = Week.getDate();
+		let WeekYear = Week.getFullYear();
+		let WeekDisplayPadded =
 			('0000' + WeekYear.toString()).slice(-4) +
 			'-' +
 			('00' + WeekMonth.toString()).slice(-2) +
@@ -142,17 +142,17 @@ function allIncluded(outputTarget = 'email') {
 		return WeekDisplayPadded;
 	}
 	function getYesterday() {
-		var today = new Date();
-		var yesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1);
-		var yesterdayMonth = yesterday.getMonth() + 1;
-		var yesterdayDay = yesterday.getDate();
-		var yesterdayYear = yesterday.getFullYear();
-		var yesterdayDisplayPadded = ('0000' + yesterdayYear.toString()).slice(-4) + '-' + ('00' + yesterdayMonth.toString()).slice(-2) + '-' + ('00' + yesterdayDay.toString()).slice(-2);
+		let today = new Date();
+		let yesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1);
+		let yesterdayMonth = yesterday.getMonth() + 1;
+		let yesterdayDay = yesterday.getDate();
+		let yesterdayYear = yesterday.getFullYear();
+		let yesterdayDisplayPadded = ('0000' + yesterdayYear.toString()).slice(-4) + '-' + ('00' + yesterdayMonth.toString()).slice(-2) + '-' + ('00' + yesterdayDay.toString()).slice(-2);
 		return yesterdayDisplayPadded;
 	}
 	// fetch github data
 	function fetchGithubData() {
-		var issueUrl = 'https://api.github.com/search/issues?q=author%3A' +
+		let issueUrl = 'https://api.github.com/search/issues?q=author%3A' +
 			githubUsername +
 			'+org%3Afossasia+created%3A' +
 			startingDate +
@@ -178,7 +178,7 @@ function allIncluded(outputTarget = 'email') {
 		});
 
 		// PR reviews fetch
-		var prUrl = 'https://api.github.com/search/issues?q=commenter%3A' +
+		let prUrl = 'https://api.github.com/search/issues?q=commenter%3A' +
 			githubUsername +
 			'+org%3Afossasia+updated%3A' +
 			startingDate +
@@ -203,7 +203,7 @@ function allIncluded(outputTarget = 'email') {
 			},
 		});
 		// fetch github user data
-		var userUrl = 'https://api.github.com/users/' + githubUsername;
+		let userUrl = 'https://api.github.com/users/' + githubUsername;
 		$.ajax({
 			dataType: 'json',
 			type: 'GET',
@@ -229,19 +229,19 @@ function allIncluded(outputTarget = 'email') {
 
 		setTimeout(() => {
 			// Generate content first
-			var lastWeekUl = '<ul>';
-			var i;
+			let lastWeekUl = '<ul>';
+			let i;
 			for (i = 0; i < lastWeekArray.length; i++) lastWeekUl += lastWeekArray[i];
 			for (i = 0; i < reviewedPrsArray.length; i++) lastWeekUl += reviewedPrsArray[i];
 			lastWeekUl += '</ul>';
 
-			var nextWeekUl = '<ul>';
+			let nextWeekUl = '<ul>';
 			for (i = 0; i < nextWeekArray.length; i++) nextWeekUl += nextWeekArray[i];
 			nextWeekUl += '</ul>';
 
 
-			var weekOrDay;
-			var weekOrDay2;
+			let weekOrDay;
+			let weekOrDay2;
 			const lastWeekRadio = document.getElementById('lastWeekContribution');
 			const yesterdayRadio = document.getElementById('yesterday');
 
@@ -301,9 +301,9 @@ ${userReason}`;
 	function getProject() {
 		if (projectName != '') return projectName;
 
-		var project = '<project name>';
-		var url = window.location.href;
-		var projectUrl = url.substr(url.lastIndexOf('/') + 1);
+		let project = '<project name>';
+		let url = window.location.href;
+		let projectUrl = url.substr(url.lastIndexOf('/') + 1);
 		if (projectUrl === 'susiai') project = 'SUSI.AI';
 		else if (projectUrl === 'open-event') project = 'Open Event';
 		return project;
@@ -311,29 +311,29 @@ ${userReason}`;
 	function scrumSubjectLoaded() {
 		if (!enableToggle) return;
 		setTimeout(() => {
-			var name = githubUserData.name || githubUsername;
-			var project = getProject();
-			var curDate = new Date();
-			var year = curDate.getFullYear().toString();
-			var date = curDate.getDate();
-			var month = curDate.getMonth();
+			let name = githubUserData.name || githubUsername;
+			let project = getProject();
+			let curDate = new Date();
+			let year = curDate.getFullYear().toString();
+			let date = curDate.getDate();
+			let month = curDate.getMonth();
 			month++;
 			if (month < 10) month = '0' + month;
 			if (date < 10) date = '0' + date;
-			var dateCode = year.toString() + month.toString() + date.toString();
+			let dateCode = year.toString() + month.toString() + date.toString();
 			scrumSubject.value = '[Scrum] ' + name + ' - ' + project + ' - ' + dateCode + ' - False';
 			scrumSubject.dispatchEvent(new Event('input', { bubbles: true }));
 		});
 	}
 
 	function writeGithubPrsReviews() {
-		var items = githubPrsReviewData.items;
+		let items = githubPrsReviewData.items;
 
 		reviewedPrsArray = [];
 		githubPrsReviewDataProcessed = {};
 
-		for (var i = 0; i < items.length; i++) {
-			var item = items[i];
+		for (let i = 0; i < items.length; i++) {
+			let item = items[i];
 			console.log(`Review item ${i + 1}/${items.length}:`, {
 				number: item.number,
 				author: item.user.login,
@@ -346,17 +346,17 @@ ${userReason}`;
 				continue;
 			}
 
-			var repository_url = item.repository_url;
-			var project = repository_url.substr(repository_url.lastIndexOf('/') + 1);
-			var title = item.title;
-			var number = item.number;
-			var html_url = item.html_url;
+			let repository_url = item.repository_url;
+			let project = repository_url.substr(repository_url.lastIndexOf('/') + 1);
+			let title = item.title;
+			let number = item.number;
+			let html_url = item.html_url;
 
 			if (!githubPrsReviewDataProcessed[project]) {
 				githubPrsReviewDataProcessed[project] = [];
 			}
 
-			var obj = {
+			let obj = {
 				number: number,
 				html_url: html_url,
 				title: title,
@@ -365,8 +365,8 @@ ${userReason}`;
 			githubPrsReviewDataProcessed[project].push(obj);
 		}
 
-		for (var repo in githubPrsReviewDataProcessed) {
-			var repoLi = '<li><i>(' + repo + ')</i> - Reviewed ';
+		for (let repo in githubPrsReviewDataProcessed) {
+			let repoLi = '<li><i>(' + repo + ')</i> - Reviewed ';
 			if (githubPrsReviewDataProcessed[repo].length > 1) {
 				repoLi += 'PRs - ';
 			} else {
@@ -374,9 +374,9 @@ ${userReason}`;
 			}
 
 			if (githubPrsReviewDataProcessed[repo].length <= 1) {
-				for (var pr in githubPrsReviewDataProcessed[repo]) {
-					var pr_arr = githubPrsReviewDataProcessed[repo][pr];
-					var prText = '';
+				for (let pr in githubPrsReviewDataProcessed[repo]) {
+					let pr_arr = githubPrsReviewDataProcessed[repo][pr];
+					let prText = '';
 					prText += `<a href='${pr_arr.html_url}' target='_blank'>#${pr_arr.number}</a> (${pr_arr.title}) `;
 					if (pr_arr.state === 'open') {
 						prText += issue_opened_button;
@@ -388,9 +388,9 @@ ${userReason}`;
 				}
 			} else {
 				repoLi += '<ul>';
-				for (var pr1 in githubPrsReviewDataProcessed[repo]) {
-					var pr_arr1 = githubPrsReviewDataProcessed[repo][pr1];
-					var prText1 = '';
+				for (let pr1 in githubPrsReviewDataProcessed[repo]) {
+					let pr_arr1 = githubPrsReviewDataProcessed[repo][pr1];
+					let prText1 = '';
 					prText1 += `<li><a href='${pr_arr1.html_url}' target='_blank'>#${pr_arr1.number}</a> (${pr_arr1.title}) `;
 					if (pr_arr1.state === 'open') {
 						prText1 += issue_opened_button;
@@ -409,14 +409,14 @@ ${userReason}`;
 		writeScrumBody();
 	}
 	function writeGithubIssuesPrs() {
-		var data = githubIssuesData;
-		var items = data.items;
+		let data = githubIssuesData;
+		let items = data.items;
 
 		lastWeekArray = [];
 		nextWeekArray = [];
 
-		for (var i = 0; i < items.length; i++) {
-			var item = items[i];
+		for (let i = 0; i < items.length; i++) {
+			let item = items[i];
 			console.log(`Processing item ${i + 1}/${items.length}:`, {
 				number: item.number,
 				title: item.title,
@@ -425,12 +425,12 @@ ${userReason}`;
 				body: item.body ? item.body.substring(0, 100) + "..." : "no body"
 			});
 
-			var html_url = item.html_url;
-			var repository_url = item.repository_url;
-			var project = repository_url.substr(repository_url.lastIndexOf('/') + 1);
-			var title = item.title;
-			var number = item.number;
-			var li = '';
+			let html_url = item.html_url;
+			let repository_url = item.repository_url;
+			let project = repository_url.substr(repository_url.lastIndexOf('/') + 1);
+			let title = item.title;
+			let number = item.number;
+			let li = '';
 
 			if (item.pull_request) {
 				if (item.state === 'closed') {
@@ -440,7 +440,7 @@ ${userReason}`;
 				}
 			} else {
 				if (item.state === 'open' && item.body && item.body.toUpperCase().indexOf('YES') > 0) {
-					var li2 = `<li><i>(${project})</i> - Work on Issue(#${number}) - <a href='${html_url}'>${title}</a> ${issue_opened_button}</li>`;
+					let li2 = `<li><i>(${project})</i> - Work on Issue(#${number}) - <a href='${html_url}'>${title}</a> ${issue_opened_button}</li>`;
 					nextWeekArray.push(li2);
 				}
 				if (item.state === 'open') {
@@ -456,7 +456,7 @@ ${userReason}`;
 		}
 		writeScrumBody();
 	}
-	var intervalBody = setInterval(() => {
+	let intervalBody = setInterval(() => {
 		if (!window.emailClientAdapter) return;
 
 		const elements = window.emailClientAdapter.getEditorElements();
@@ -467,7 +467,7 @@ ${userReason}`;
 		writeScrumBody();
 	}, 500);
 
-	var intervalSubject = setInterval(() => {
+	let intervalSubject = setInterval(() => {
 		if (!githubUserData || !window.emailClientAdapter) return;
 
 		const elements = window.emailClientAdapter.getEditorElements();
@@ -479,31 +479,31 @@ ${userReason}`;
 	}, 500);
 
 	//check for github safe writing
-	var intervalWriteGithub = setInterval(() => {
+	let intervalWriteGithub = setInterval(() => {
 		if (scrumBody && githubUsername && githubIssuesData) {
 			clearInterval(intervalWriteGithub);
 			writeGithubIssuesPrs();
 		}
 	}, 500);
 	//check for github prs reviews safe writing
-	var intervalWriteGithubReviews = setInterval(() => {
+	let intervalWriteGithubReviews = setInterval(() => {
 		if (scrumBody && githubUsername && githubPrsReviewData) {
 			clearInterval(intervalWriteGithubReviews);
 			writeGithubPrsReviews();
 		}
 	}, 500);
 	if (!refreshButton_Placed) {
-		var intervalWriteButton = setInterval(() => {
+		let intervalWriteButton = setInterval(() => {
 			if (document.getElementsByClassName('F0XO1GC-x-b').length == 3 && scrumBody && enableToggle) {
 				refreshButton_Placed = true;
 				clearInterval(intervalWriteButton);
-				var td = document.createElement('td');
-				var button = document.createElement('button');
+				let td = document.createElement('td');
+				let button = document.createElement('button');
 				button.style = 'background-image:none;background-color:#3F51B5;';
 				button.setAttribute('class', 'F0XO1GC-n-a F0XO1GC-G-a');
 				button.title = 'Rewrite your SCRUM using updated settings!';
 				button.id = 'refreshButton';
-				var elemText = document.createTextNode('↻ Rewrite SCRUM!');
+				let elemText = document.createTextNode('↻ Rewrite SCRUM!');
 				button.appendChild(elemText);
 				td.appendChild(button);
 				document.getElementsByClassName('F0XO1GC-x-b')[0].children[0].children[0].appendChild(td);
