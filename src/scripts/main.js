6957a33920d3ab217fa8a14fec13eb7a516fcc3a
Vedansh Saini <77830698+vedansh-5@users.noreply.github.com>
2025-05-28 23:17:09 +0530

    replaced var with let for better scope indexing
    Signed-off-by: Vedansh Saini <77830698+vedansh-5@users.noreply.github.com>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
 /* global $,Materialize*/
-var enableToggleElement = document.getElementById('enable');
-var githubUsernameElement = document.getElementById('githubUsername');
-var projectNameElement = document.getElementById('projectName');
-var lastWeekContributionElement = document.getElementById('lastWeekContribution');
-var startingDateElement = document.getElementById('startingDate');
-var endingDateElement = document.getElementById('endingDate');
-var showOpenLabelElement = document.getElementById('showOpenLabel');
-var userReasonElement = document.getElementById('userReason');
-var gsoc = 0; //0 means gsoc. 1 means gsoc
+let enableToggleElement = document.getElementById('enable');
+let githubUsernameElement = document.getElementById('githubUsername');
+let projectNameElement = document.getElementById('projectName');
+let lastWeekContributionElement = document.getElementById('lastWeekContribution');
+let startingDateElement = document.getElementById('startingDate');
+let endingDateElement = document.getElementById('endingDate');
+let showOpenLabelElement = document.getElementById('showOpenLabel');
+let userReasonElement = document.getElementById('userReason');
+let gsoc = 0; //0 means gsoc. 1 means gsoc
 function handleBodyOnLoad() {
 	// prefill name
 	chrome.storage.local.get(
@@ -79,19 +79,19 @@ function handleBodyOnLoad() {
 	);
 }
 function handleEnableChange() {
-	var value = enableToggleElement.checked;
+	let value = enableToggleElement.checked;
 	chrome.storage.local.set({ enableToggle: value });
 }
 function handleStartingDateChange() {
-	var value = startingDateElement.value;
+	let value = startingDateElement.value;
 	chrome.storage.local.set({ startingDate: value });
 }
 function handleEndingDateChange() {
-	var value = endingDateElement.value;
+	let value = endingDateElement.value;
 	chrome.storage.local.set({ endingDate: value });
 }
 function handleLastWeekContributionChange() {
-	var value = lastWeekContributionElement.checked;
+	let value = lastWeekContributionElement.checked;
 	if (value) {
 		startingDateElement.disabled = true;
 		endingDateElement.disabled = true;
@@ -106,13 +106,13 @@ function handleLastWeekContributionChange() {
 	chrome.storage.local.set({ lastWeekContribution: value });
 }
 function getLastWeek() {
-	var today = new Date();
-	var noDays_to_goback = gsoc == 0 ? 7 : 1;
-	var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - noDays_to_goback);
-	var lastWeekMonth = lastWeek.getMonth() + 1;
-	var lastWeekDay = lastWeek.getDate();
-	var lastWeekYear = lastWeek.getFullYear();
-	var lastWeekDisplayPadded =
+	let today = new Date();
+	let noDays_to_goback = gsoc == 0 ? 7 : 1;
+	let lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - noDays_to_goback);
+	let lastWeekMonth = lastWeek.getMonth() + 1;
+	let lastWeekDay = lastWeek.getDate();
+	let lastWeekYear = lastWeek.getFullYear();
+	let lastWeekDisplayPadded =
 		('0000' + lastWeekYear.toString()).slice(-4) +
 		'-' +
 		('00' + lastWeekMonth.toString()).slice(-2) +
@@ -121,12 +121,12 @@ function getLastWeek() {
 	return lastWeekDisplayPadded;
 }
 function getToday() {
-	var today = new Date();
-	var Week = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-	var WeekMonth = Week.getMonth() + 1;
-	var WeekDay = Week.getDate();
-	var WeekYear = Week.getFullYear();
-	var WeekDisplayPadded =
+	let today = new Date();
+	let Week = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+	let WeekMonth = Week.getMonth() + 1;
+	let WeekDay = Week.getDate();
+	let WeekYear = Week.getFullYear();
+	let WeekDisplayPadded =
 		('0000' + WeekYear.toString()).slice(-4) +
 		'-' +
 		('00' + WeekMonth.toString()).slice(-2) +
@@ -136,20 +136,20 @@ function getToday() {
 }
 
 function handleGithubUsernameChange() {
-	var value = githubUsernameElement.value;
+	let value = githubUsernameElement.value;
 	chrome.storage.local.set({ githubUsername: value });
 }
 function handleProjectNameChange() {
-	var value = projectNameElement.value;
+	let value = projectNameElement.value;
 	chrome.storage.local.set({ projectName: value });
 }
 function handleOpenLabelChange() {
-	var value = showOpenLabelElement.checked;
+	let value = showOpenLabelElement.checked;
 	chrome.storage.local.set({ showOpenLabel: value });
 	chrome.storage.local.set({ showClosedLabel: value });
 }
 function handleUserReasonChange() {
-	var value = userReasonElement.value;
+	let value = userReasonElement.value;
 	chrome.storage.local.set({ userReason: value });
 }
 function handleCodeheatClick() {
