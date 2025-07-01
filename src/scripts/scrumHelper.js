d0d45bef4a315908fe42017b7f731e79fd31e4e9
Vedansh Saini <77830698+vedansh-5@users.noreply.github.com>
2025-07-02 00:25:13 +0530

    custom date
    Signed-off-by: Vedansh Saini <77830698+vedansh-5@users.noreply.github.com>

--- src/scripts/scrumHelper.js
+++ src/scripts/scrumHelper.js
@@ -119,11 +119,11 @@ function allIncluded(outputTarget = 'email') {
 					handleLastWeekContributionChange();
 				} else if (items.yesterdayContribution) {
 					handleYesterdayContributionChange();
-				} else if (items.startDate && items.endingDate) {
+				} else if (items.startingDate && items.endingDate) {
 					startingDate = items.startingDate;
 					endingDate = items.endingDate;
 				} else {
-					handleLastWeekContributionChange(); //when no date is stored, i.e on fresh unpack - default to last week.
+					handleLastWeekContributionChange(); //on fresh unpack - default to last week.
 					if (outputTarget === 'popup') {
 						chrome.storage.local.set({ lastWeekContribution: true, yesterdayContribution: false });
 					}
