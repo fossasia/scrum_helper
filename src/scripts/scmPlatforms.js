f7be5473d921efd9cd119ed78a52a2c030ca6b5f
PhilixTheExplorer <nwaimarn@gmail.com>
2026-05-11 18:55:41 +0700

    refactor: add SCM provider registry skeleton

--- src/scripts/scmPlatforms.js
+++ src/scripts/scmPlatforms.js
@@ -3,6 +3,32 @@ const SCM_PLATFORMS = Object.freeze({
 	GITLAB: 'gitlab',
 });
 
+const SCM_PROVIDER_REGISTRY = Object.freeze({
+	[SCM_PLATFORMS.GITHUB]: Object.freeze({
+		platformId: SCM_PLATFORMS.GITHUB,
+		displayName: 'GitHub',
+		iconClass: 'fab fa-github',
+		usernameLabelI18nKey: 'githubUsernameLabel',
+		visibleSettingsSection: 'github',
+	}),
+	[SCM_PLATFORMS.GITLAB]: Object.freeze({
+		platformId: SCM_PLATFORMS.GITLAB,
+		displayName: 'GitLab',
+		iconClass: 'fab fa-gitlab',
+		usernameLabelI18nKey: 'gitlabUsernameLabel',
+		visibleSettingsSection: 'gitlab',
+	}),
+});
+
+const SCM_PROVIDER_IDS = Object.freeze([SCM_PLATFORMS.GITHUB, SCM_PLATFORMS.GITLAB]);
+
+function getScmProvider(platformId) {
+	return SCM_PROVIDER_REGISTRY[platformId] || SCM_PROVIDER_REGISTRY[SCM_PLATFORMS.GITHUB];
+}
+
 if (typeof window !== 'undefined') {
 	window.SCM_PLATFORMS = SCM_PLATFORMS;
+	window.SCM_PROVIDER_REGISTRY = SCM_PROVIDER_REGISTRY;
+	window.SCM_PROVIDER_IDS = SCM_PROVIDER_IDS;
+	window.getScmProvider = getScmProvider;
 }
