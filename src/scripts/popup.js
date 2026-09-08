613bda482fec138764fe0348dd3f48de84514f01
Mateeb Haider <b-27240@student.usa.edu.pk>
2026-09-08 14:57:55 +0500

    chore: update platform selction span to full width (#842)
    * chore: update platform selction div width
    
    * ref: keep icons size to unchange
    
    ---------
    
    Co-authored-by: Saksham Sirohi <sakshamsirohi27@gmail.com>

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -2401,7 +2401,7 @@ function setPlatformDropdown(value) {
 			dropdownSelected.innerHTML = '<i class="fab fa-gitlab mr-2"></i> GitLab';
 		} else if (value === 'codeberg') {
 			dropdownSelected.innerHTML = `
-				<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="width: 18px; height: 18px; display: inline-block; vertical-align: middle; margin-right: 8px; fill: currentColor;">
+				<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="width: 15px; height: 15px; display: inline-block; vertical-align: middle; margin-right: 8px; fill: currentColor;">
 					<title>Codeberg</title>
 					<path d="M11.999.747A11.974 11.974 0 0 0 0 12.75c0 2.254.635 4.465 1.833 6.376L11.837 6.19c.072-.092.251-.092.323 0l4.178 5.402h-2.992l.065.239h3.113l.882 1.138h-3.674l.103.374h3.86l.777 1.003h-4.358l.135.483h4.593l.695.894h-5.038l.165.589h5.326l.609.785h-5.717l.182.65h6.038l.562.727h-6.397l.183.65h6.717A12.003 12.003 0 0 0 24 12.75 11.977 11.977 0 0 0 11.999.747zm3.654 19.104.182.65h5.326c.173-.204.353-.433.513-.65zm.385 1.377.18.65h3.563c.233-.198.485-.428.712-.65zm.383 1.377.182.648h1.203c.356-.204.685-.412 1.042-.648z"/>
 				</svg> Codeberg`;
@@ -2546,7 +2546,7 @@ browser.storage.local.get(['platform']).then((result) => {
 			dropdownSelected.innerHTML = '<i class="fab fa-gitlab mr-2"></i> GitLab';
 		} else if (platform === 'codeberg') {
 			dropdownSelected.innerHTML = `
-				<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="width: 18px; height: 18px; display: inline-block; vertical-align: middle; margin-right: 8px; fill: currentColor;">
+				<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="width: 15px; height: 15px; display: inline-block; vertical-align: middle; margin-right: 8px; fill: currentColor;">
 					<title>Codeberg</title>
 					<path d="M11.999.747A11.974 11.974 0 0 0 0 12.75c0 2.254.635 4.465 1.833 6.376L11.837 6.19c.072-.092.251-.092.323 0l4.178 5.402h-2.992l.065.239h3.113l.882 1.138h-3.674l.103.374h3.86l.777 1.003h-4.358l.135.483h4.593l.695.894h-5.038l.165.589h5.326l.609.785h-5.717l.182.65h6.038l.562.727h-6.397l.183.65h6.717A12.003 12.003 0 0 0 24 12.75 11.977 11.977 0 0 0 11.999.747zm3.654 19.104.182.65h5.326c.173-.204.353-.433.513-.65zm.385 1.377.18.65h3.563c.233-.198.485-.428.712-.65zm.383 1.377.182.648h1.203c.356-.204.685-.412 1.042-.648z"/>
 				</svg> Codeberg`;
